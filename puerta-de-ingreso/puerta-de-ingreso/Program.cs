156276279270244042b5b11635b0bfd549195ad7
Hernan Casado <hcasado@gmail.com>
2021-09-18 11:17:23 -0300

    calcular el perimetro de un cuadrado

---
+++ puerta-de-ingreso/puerta-de-ingreso/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace puerta_de_ingreso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcularPerimetroDeUnCuadrado();
+            //CalcularPerimetroDeUnTriangulo();
+        }
+
+        private static void CalcularPerimetroDeUnCuadrado()
+        {
+            string lado = "";
+
+            Console.WriteLine("Vamos a calcular el perimetro de un cuadrado!");
+            Console.WriteLine("Cuanto mide uno de los lados?");
+            lado = Console.ReadLine();
+
+            long ladoIntTry;
+            if (long.TryParse(lado, out ladoIntTry))
+            {
+                var perimetro = 4 * (int)ladoIntTry;
+                Console.WriteLine("El perimetro es " + perimetro.ToString());
+
+            }
+            else
+            {
+                Console.WriteLine("No es un valor válido para lado");
+            }
+        }
+
+    }
+}
