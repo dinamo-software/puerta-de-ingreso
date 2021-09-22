3ec96080f23d2d3eaff3f1d614762fc26ccb6856
Leandro_Tonello <leandronicolas1177@gmail.com>
2021-09-22 11:33:20 -0300

    Agregados Area y perimetro del Triangulo

--- puerta-de-ingreso/puerta-de-ingreso/Program.cs
+++ puerta-de-ingreso/puerta-de-ingreso/Program.cs
@@ -6,8 +6,9 @@ namespace puerta_de_ingreso
     {
         static void Main(string[] args)
         {
-            CalcularPerimetroDeUnCuadrado();
-            //CalcularPerimetroDeUnTriangulo();
+           CalcularPerimetroDeUnCuadrado();
+           CalcularPerimetroDeUnTriangulo();
+           CalcularAreaDeUnTriangulo();
         }
 
         private static void CalcularPerimetroDeUnCuadrado()
@@ -30,6 +31,57 @@ namespace puerta_de_ingreso
                 Console.WriteLine("No es un valor válido para lado");
             }
         }
+        
+        private static void CalcularPerimetroDeUnTriangulo()
+        {
+            string lado = "";
+            string lado2 = "";
+            string lado3 = "";
 
-    }
+            Console.WriteLine("Vamos a calcular el perimetro de un Triangulo!");
+            Console.WriteLine("Cuanto miden los 3 lados?");
+            lado = Console.ReadLine();
+            lado2 = Console.ReadLine();
+            lado3 = Console.ReadLine();
+
+            long ladoIntTry;
+            long ladoIntTry2;
+            long ladoIntTry3;
+            
+            if (long.TryParse(lado, out ladoIntTry) && long.TryParse(lado2, out ladoIntTry2)&& long.TryParse(lado3, out ladoIntTry3))
+            {
+                var perimetro =  (int)ladoIntTry + (int)ladoIntTry2 + (int)ladoIntTry3 ;
+                Console.WriteLine("El perimetro es " + perimetro.ToString());
+
+            }
+            else
+            {
+                Console.WriteLine("No es un valor válido para lado");
+            }
+        }
+        
+        private static void CalcularAreaDeUnTriangulo()
+        {
+            string basee = "";
+            string altura = "";
+            Console.WriteLine("Vamos a calcular el Area de un triango");
+            Console.WriteLine("cuando mide la base?");
+            basee = Console.ReadLine();
+            Console.WriteLine("cuando mide la altura?");
+            altura = Console.ReadLine();
+            long baseIntTry;
+            long alturaIntTry;
+            if (long.TryParse(basee, out baseIntTry) && (long.TryParse(altura, out alturaIntTry)))
+            {
+                var area = ((int) baseIntTry * (int) alturaIntTry) / 2 ;
+                Console.WriteLine("El Area es " + area.ToString() );
+            }
+            else
+            {
+                Console.WriteLine("No es un valor válido para lado");
+            }
+        }
+        }
 }
+    
+
